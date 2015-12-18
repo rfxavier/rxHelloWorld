bed3dc487e363bf1371c877ec1a7b8bfccc3d53c
RenatoFX <renatoferreiraxavier@gmail.com>
2015-12-17 22:23:07 -0200

    working checkpoint

--- src/modules/model.js
+++ src/modules/model.js
@@ -7,29 +7,38 @@ var $ = require('jquery');
 var model = module.exports = {
     viewModel: {
         name: ko.observable(),
-        players: ko.observableArray()
+        players: ko.observableArray(),
+        inputFirstName: ko.observable(),
+        inputLastName: ko.observable(),
+        //playerData : {
+        //    firstName: viewModel.inputFirstName,
+        //    lastName: viewModel.inputLastName
+        //},
+        showViewModel: function () {
+            console.log(ko.toJSON(model.viewModel));
+        }
     },
 
     init: function () {
         console.log('init model2');
-
-        model.getPlayers().done(function(data, textStatus, jqXHR) {
-                model.viewModel.players(data);
-                console.log(model.viewModel.players);
-            })
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                console.log(textStatus + "; " + errorThrown)
-            });
+        model.getPlayers();
     },
 
     getPlayers: function() {
-        return $.ajax({
+         $.ajax({
             url: 'http://rfxavier-001-site3.btempurl.com/api/player',
             type: 'GET',
             dataType: 'jsonp',
             data: {},
             cache: false
-        });
+        })
+             .done(function(data, textStatus, jqXHR) {
+                 model.viewModel.players(data);
+                 console.log(model.viewModel.players());
+             })
+             .fail(function(jqXHR, textStatus, errorThrown) {
+                 console.log(textStatus + "; " + errorThrown)
+             });
     },
 
     applyKoBindings: function () {
